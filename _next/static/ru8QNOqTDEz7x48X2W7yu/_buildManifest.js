c4c6833048efccae03200483656114dde2506a13
董建豪 <2995251733@qq.com>
2023-02-02 15:25:35 +0800

    fix(js dir): _next->js

--- _next/static/ru8QNOqTDEz7x48X2W7yu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4cee64cba4615aa2.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],"/[id]":["static/chunks/pages/[id]-0edb7dfc76363b09.js"],sortedPages:["/","/_app","/_error","/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
